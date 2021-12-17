2a42c963f253b80907217ad78a91916b94f08538
lballesterosm <leonardo.ballesteros.com>
2021-12-17 15:48:57 -0300

    new files

---
+++ vCloud/vCloudDHCPSubnet/main.tf
@@ -0,0 +1,42 @@
+terraform {
+  required_providers {
+    vcd = {
+      version = "3.4.0"
+    }
+  }
+}
+
+variable "subnet_name" {
+
+}
+
+variable "dhcp_start_ip" {
+
+}
+variable "dhcp_end_ip" {
+
+}
+
+variable "edge_gw" {
+
+}
+
+variable "dns_1" {
+
+}
+
+resource "vcd_network_routed" "net" {
+  org = "dae691dbea51489088e89e813ba339b9"
+  vdc ="vmware-dc"
+  name = var.subnet_name
+  edge_gateway = var.edge_gw
+  gateway = "172.168.2.1"
+  dns1 = "172.168.2.1"
+  
+  
+  dhcp_pool {
+    start_address = var.dhcp_start_ip
+    end_address   = var.dhcp_end_ip
+  }
+
+}
