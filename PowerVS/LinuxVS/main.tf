c88bbe686c45107078cb95dbd85fae136a5fbc43
lballesterosm <leonardo.ballesteros.com>
2021-12-20 16:06:40 -0300

    first test

--- PowerVS/LinuxVS/main.tf
+++ PowerVS/LinuxVS/main.tf
@@ -5,29 +5,68 @@ provider "ibm" {
 
 # Variables
 
-variable "test_key_1" {
+variable "power_instance_name" {
 
 }
 
-# Create an IBM Cloud infrastructure SSH key. You can find the SSH key surfaces in the infrastructure console under Devices > Manage > SSH Keys
-resource "ibm_compute_ssh_key" "test_key_1" {
-  label      = "test_key_1"
-  public_key = var.ssh_public_key
+variable "power_replicas" {
+
 }
+
+variable "power_vm_pinning" {
+
+}
+
+variable "power_placement_group" {
+
+}
+
+variable "power_ssh_key" {
+
+}
+
+variable "power_image" {
+
+}
+
+variable "power_replicas" {
+
+}
+
+variable "power_network" {
+
+}
+
+variable "power_machine_type" {
+
+}
+
+variable "power_core_type" {
+
+}
+
+variable "power_cores" {
+
+}
+
+variable "power_memory" {
+
+}
+
+
 
 # Create a Power virtual server instance
 resource "ibm_pi_instance" "test-instance" {
-    pi_memory             = "4"
-    pi_processors         = "2"
-    pi_instance_name      = "test-vm"
-    pi_proc_type          = "shared"
-    pi_image_id           = "${data.ibm_pi_image.powerimages.id}"
-    pi_key_pair_name      = ibm_pi_key.key.key_id
-    pi_sys_type           = "s922"
-    pi_cloud_instance_id  = "51e1879c-bcbe-4ee1-a008-49cdba0eaf60"
-    pi_pin_policy         = "none"
-    pi_health_status      = "WARNING"
+    pi_instance_name      = var.power_instance_name
+    pi_memory             = var.power_memory
+    pi_processors         = var.power_cores
+        pi_proc_type          = var.power_core_type
+    pi_image_id           = var.power_image
+    pi_key_pair_name      = var.power_ssh_key
+    pi_sys_type           = var.power_machine_type
+    pi_pin_policy         = var.power_vm_pinning
+    
     pi_network {
-      network_id = data.ibm_pi_public_network.dsnetwork.id
+      network_id = var.power_network
     }
 }
