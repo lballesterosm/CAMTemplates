c4f3ac3fd62f7d7ff7a59c18ef3b7c7b7e1539f8
lballesterosm <leonardo.ballesteros.com>
2021-12-20 20:27:44 -0300

    image id updat

--- PowerVS/LinuxVS/main.tf
+++ PowerVS/LinuxVS/main.tf
@@ -55,13 +55,18 @@ data "ibm_pi_network" "power_networks" {
     pi_cloud_instance_id = "c2af394e-33b8-4154-9275-f69f5d65dc0d"
 }
 
+data "ibm_pi_image" "power_images" {
+    pi_image_name      = var.power_image
+    pi_cloud_instance_id = "c2af394e-33b8-4154-9275-f69f5d65dc0d"
+}
+
 # Create a Power virtual server instance
 resource "ibm_pi_instance" "VirtualServerInstance" {
     pi_instance_name      = var.power_instance_name
     pi_memory             = var.power_memory
     pi_processors         = var.power_cores
     pi_proc_type          = var.power_core_type
-    pi_image_id           = var.power_image
+    pi_image_id           = ["${data.ibm_pi_image.power_images.id}"]
     pi_key_pair_name      = var.power_ssh_key
     pi_sys_type           = var.power_machine_type
     pi_pin_policy         = var.power_vm_pinning
