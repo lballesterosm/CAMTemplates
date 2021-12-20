246ccdfa63b51e9f114b1b193a2bdba2a2d28785
lballesterosm <leonardo.ballesteros.com>
2021-12-20 14:56:28 -0300

    first files

--- PowerVS/AIX/main.tf
+++ PowerVS/AIX/main.tf
@@ -1,1 +1,34 @@
+# Configure the IBM Provider
+provider "ibm" {
+  region = "us-south"
+}
+
+# Variables
+
+variable "test_key_1" {
+
+}
+
+# Create an IBM Cloud infrastructure SSH key. You can find the SSH key surfaces in the infrastructure console under Devices > Manage > SSH Keys
+resource "ibm_compute_ssh_key" "test_key_1" {
+  label      = "test_key_1"
+  public_key = var.ssh_public_key
+}
+
+# Create a Power virtual server instance
+resource "ibm_pi_instance" "test-instance" {
+    pi_memory             = "4"
+    pi_processors         = "2"
+    pi_instance_name      = "test-vm"
+    pi_proc_type          = "shared"
+    pi_image_id           = "${data.ibm_pi_image.powerimages.id}"
+    pi_key_pair_name      = ibm_pi_key.key.key_id
+    pi_sys_type           = "s922"
+    pi_cloud_instance_id  = "51e1879c-bcbe-4ee1-a008-49cdba0eaf60"
+    pi_pin_policy         = "none"
+    pi_health_status      = "WARNING"
+    pi_network {
+      network_id = data.ibm_pi_public_network.dsnetwork.id
+    }
+}
 
