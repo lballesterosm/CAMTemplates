5ab2fbffa2b78b1eb7649ccaf93a9fca546caeb0
lballesterosm <leonardo.ballesteros.com>
2021-12-20 20:00:34 -0300

    main update

--- PowerVS/LinuxVS/main.tf
+++ PowerVS/LinuxVS/main.tf
@@ -1,6 +1,7 @@
 # Configure the IBM Provider
 provider "ibm" {
   region = "us-south"
+  zone   = "dal10"
 }
 
 # Variables
@@ -49,20 +50,25 @@ variable "power_memory" {
 
 }
 
-
+data "ibm_pi_network" "power_networks" {
+    count                = "${length(var.power_network)}"
+    pi_network_name      = "${var.power_network[count.index]}"
+    pi_cloud_instance_id = "c2af394e-33b8-4154-9275-f69f5d65dc0d"
+}
 
 # Create a Power virtual server instance
-resource "ibm_pi_instance" "test-instance" {
+resource "ibm_pi_instance" "VirtualServerInstance" {
     pi_instance_name      = var.power_instance_name
     pi_memory             = var.power_memory
     pi_processors         = var.power_cores
-        pi_proc_type          = var.power_core_type
+    pi_proc_type          = var.power_core_type
     pi_image_id           = var.power_image
     pi_key_pair_name      = var.power_ssh_key
     pi_sys_type           = var.power_machine_type
     pi_pin_policy         = var.power_vm_pinning
+    pi_cloud_instance_id  = "c2af394e-33b8-4154-9275-f69f5d65dc0d"
+    pi_network_ids        = ["${data.ibm_pi_network.power_networks.*.networkid}"]
+    pi_replicants         = var.power_replicas
     
-    pi_network {
-      network_id = var.power_network
-    }
+
 }
