e6f56521f98c1f5a3049157f792944f88f74c9a7
lballesterosm <61248571+lballesterosm@users.noreply.github.com>
2022-01-17 14:12:40 -0300

    new full ocp4 contanier

---
+++ vCloud/ocp-2workers/script/helper_install.sh
@@ -0,0 +1,7 @@
+cd $HOME
+#yum -y install "https://dl.fedoraproject.org/pub/epel/epel-release-latest-$(rpm -E %rhel).noarch.rpm"
+yum -y install https://dl.fedoraproject.org/pub/epel/epel-release-latest-7.noarch.rpm
+yum -y install ansible git
+git clone https://github.com/RedHatOfficial/ocp4-helpernode
+cd ocp4-helpernode
+cp docs/examples/vars.yaml .
