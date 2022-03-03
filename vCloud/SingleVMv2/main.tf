0c80fe62b1b05b2d26906045cb9d2b3c3844e0d3
lballesterosm <61248571+lballesterosm@users.noreply.github.com>
2022-03-03 13:38:16 -0300

    user and pass on main.tf

--- vCloud/SingleVMv2/main.tf
+++ vCloud/SingleVMv2/main.tf
@@ -18,7 +18,9 @@ variable "vm_1_subnet" {
 
 
 provider "vcd" {
-  version              = "3.4.0"
+  user      = "admin"
+  password  = "JeNDT5CXQUVSJJrKT"
+  version   = "3.4.0"
   org       = "dae691dbea51489088e89e813ba339b9"
   url       = "https://daldir01.vmware-solutions.cloud.ibm.com/api"  
 }
